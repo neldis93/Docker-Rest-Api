3c7db91cb08278198914a2d3682203d2046545fb
Neldis Barrios <neldis12@hotmail.com>
2021-11-04 10:39:38 +0100

    Syntax and package

--- app/books/views.py
+++ app/books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .serializers import BooksSerializer
+from .models import Books
 from rest_framework.generics import (
     ListAPIView, 
     CreateAPIView, 
@@ -9,40 +10,27 @@ from rest_framework.generics import (
     RetrieveUpdateAPIView,
 )
 
-from .serializers import BooksSerializer
-
-from .models import Books
-
-
 class BooksListApiView(ListAPIView):
-
-    serializer_class= BooksSerializer
-
+    serializer_class = BooksSerializer
+    
     def get_queryset(self):
         return Books.objects.all()
 
 class BooksCreateApiView(CreateAPIView):
-
-     serializer_class=BooksSerializer
-
+     serializer_class = BooksSerializer
 
 class BooksRetrieveAPIView(RetrieveAPIView):
-
-    serializer_class=BooksSerializer
+    serializer_class = BooksSerializer
     queryset=Books.objects.all()
 
 class BooksDeleteAPIView(DestroyAPIView):
-
-    serializer_class=BooksSerializer
+    serializer_class = BooksSerializer
     queryset=Books.objects.all()
 
 class BooksUpdateAPIView(UpdateAPIView):
-
-    serializer_class=BooksSerializer
+    serializer_class = BooksSerializer
     queryset=Books.objects.all()
 
-
 class BooksModifyAPIView(RetrieveUpdateAPIView):
-
-    serializer_class=BooksSerializer
+    serializer_class = BooksSerializer
     queryset=Books.objects.all()
