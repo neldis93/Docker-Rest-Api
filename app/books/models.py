215cd3ace9bb925d55fa4c6487edabbbeee9a0a3
Neldis Barrios <neldis12@hotmail.com>
2021-11-04 10:35:51 +0100

    Syntax

--- app/books/models.py
+++ app/books/models.py
@@ -1,15 +1,12 @@
-# nos indica la fecha de creacion y la fecha de modificacion
+# indicates the date of creation and the date of modification
 from model_utils.models import TimeStampedModel
-
 from django.db import models
 
 class Books(TimeStampedModel):
-
     title=models.CharField(max_length=200)
     author=models.CharField(max_length=50)
     length=models.IntegerField()
     price=models.FloatField()
-
 
     class Meta:
         verbose_name='Book'
